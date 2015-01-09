2df3031a48cb7ce759c7614f9d77bb8899848dc8
Benjamin Pearce <hawk.git@bcpearce.com>
2015-01-09 00:18:25 -0500

    made setting the year to a blank result in nil list

--- lib/edmunds.rb
+++ lib/edmunds.rb
@@ -4,6 +4,7 @@ module Edmunds
   VEHICLE = "/api/vehicle/v2"
 
   def self.list_of_auto_makes(options)
+    options[:params][:year] = "1800" if options[:params][:year] == ""
     raw_makes = RestClient.get(BASE_URI + VEHICLE + "/makes", options)
     JSON.parse(raw_makes)["makes"].map { |x| x["name"] }
   end
--- spec/lib/edmunds_spec.rb
+++ spec/lib/edmunds_spec.rb
@@ -40,6 +40,21 @@ describe "self.list_of_auto_makes", vcr:true do
       expect(@makes_from_2005 - @all_makes).to be_empty
       expect(@all_makes.length).to be > @makes_from_2005.length
     end
+
+  end
+
+  context "passing nil for year" do
+    before do
+      VCR.use_cassette 'edmunds_makes_from_blank' do
+        params = { params: { api_key:Rails.application.secrets.edmunds_api_key,
+                             year:"" } }
+        @makes_from_blank = Edmunds.list_of_auto_makes(params)
+      end
+    end
+
+    it "should return a blank list" do
+      expect(@makes_from_blank).to be_empty
+    end
   end
 
 end
