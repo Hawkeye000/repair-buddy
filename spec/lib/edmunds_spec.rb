ee948ab3433032d82507aef5dd56d7977bada0dc
Benjamin Pearce <hawk.git@bcpearce.com>
2015-01-08 22:47:07 -0500

    added test for a car brand existence

--- spec/lib/edmunds_spec.rb
+++ spec/lib/edmunds_spec.rb
@@ -2,11 +2,18 @@ require 'rails_helper'
 
 describe "self.list_of_auto_makes", vcr:true do
 
-  it "should return an array of strings" do
+  before do
     VCR.use_cassette 'edmunds_makes' do
-      makes = Edmunds.list_of_auto_makes(params:{ api_key:Rails.application.secrets.edmunds_api_key })
-      makes.each { |make| expect(make).to be_a String }
+      @makes = Edmunds.list_of_auto_makes(params:{ api_key:Rails.application.secrets.edmunds_api_key })
     end
+  end
+
+  it "should return an array of strings" do
+    @makes.each { |make| expect(make).to be_a String }
+  end
+
+  it "should include 'Acura'" do
+    expect(@makes).to include('Acura')
   end
 
 end
