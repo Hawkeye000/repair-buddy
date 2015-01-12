22d541d4fb0b2c9ef1908602a9f0c5c800a51852
Benjamin Pearce <hawk.git@bcpearce.com>
2015-01-11 20:30:57 -0500

    added presence validators for car attributes

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,4 +3,10 @@ class Car < ActiveRecord::Base
   belongs_to :user
   has_many :records
 
+  validates :year, presence:true
+  validates :make, presence:true
+  validates :model, presence:true
+  validates :trim, presence:true
+  validates :edmunds_id, presence:true
+
 end
--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Car, :type => :model do
-  it { should have_many :records }
-  it { should belong_to :user }
+  describe "associations" do
+    it { should have_many :records }
+    it { should belong_to :user }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of :edmunds_id }
+    it { should validate_presence_of :make }
+    it { should validate_presence_of :model }
+    it { should validate_presence_of :year }
+    it { should validate_presence_of :trim }
+  end
 end
