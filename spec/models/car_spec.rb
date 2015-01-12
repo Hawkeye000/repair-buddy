70bf2531ab31e8f07ba276df3af324314acdfcf2
Benjamin Pearce <hawk.git@bcpearce.com>
2015-01-11 20:03:29 -0500

    added association tests for car

--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Car, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many :records }
+  it { should belong_to :user }
 end
