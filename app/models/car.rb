766178347398ec3e971c5699c5d59274d5c7735d
Benjamin Pearce <hawk.git@bcpearce.com>
2015-01-11 20:35:48 -0500

    added validations for car parameters

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,10 +3,11 @@ class Car < ActiveRecord::Base
   belongs_to :user
   has_many :records
 
-  validates :year, presence:true
+  validates :year, presence:true, format: /\A\d{4}\z/
   validates :make, presence:true
   validates :model, presence:true
   validates :trim, presence:true
   validates :edmunds_id, presence:true
+  validates :user_id, presence:true
 
 end
--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -7,10 +7,18 @@ RSpec.describe Car, :type => :model do
   end
 
   describe "validations" do
-    it { should validate_presence_of :edmunds_id }
-    it { should validate_presence_of :make }
-    it { should validate_presence_of :model }
-    it { should validate_presence_of :year }
-    it { should validate_presence_of :trim }
+    describe "presence validators" do
+      it { should validate_presence_of :edmunds_id }
+      it { should validate_presence_of :make }
+      it { should validate_presence_of :model }
+      it { should validate_presence_of :year }
+      it { should validate_presence_of :trim }
+      it { should validate_presence_of :user_id }
+    end
+    describe "year format" do
+      it { should_not allow_value(21000).for(:year) }
+      it { should_not allow_value(381).for(:year) }
+      it { should allow_value(2004).for(:year) }
+    end
   end
 end
