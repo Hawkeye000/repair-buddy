6743351d7c74eee849bdb8bcbca4ef6089dd98e7
Benjamin Pearce <hawk.git@bcpearce.com>
2015-01-14 22:43:41 -0500

    added link to 'Add Car' to the navbar

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -2,22 +2,29 @@ require 'rails_helper'
 
 describe "home page" do
 
+  let(:user) { create(:user) }
   before { visit '/' }
 
   context "user is not logged in" do
     it "should have a link to login" do
       expect(page).to have_link("Login", href:new_user_session_path)
     end
+    it "should not have a link to 'Add Car'" do
+      expect(page).to_not have_link("Add Car", href:new_user_car_path(user))
+    end
   end
   context "user is logged in" do
+
     before do
-      @user = create(:user)
-      login_as @user, scope: :user
+      login_as user, scope: :user
       visit current_path
     end
 
     it "should have a link to Profile" do
-      expect(page).to have_link("Profile", href:user_path(@user))
+      expect(page).to have_link("Profile", href:user_path(user))
+    end
+    it "should have a link to 'Add Car'" do
+      expect(page).to have_link("Add Car", href:new_user_car_path(user))
     end
   end
 
