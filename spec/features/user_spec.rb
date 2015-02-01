710f0bb77e39e1989808584734b430e590515ff4
Benjamin Pearce <hawk.git@bcpearce.com>
2015-02-01 17:07:26 -0500

    fixed user features spec scoping

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,21 +1,23 @@
 require 'rails_helper'
 
-describe 'Signing up new Users' do
+describe 'User Features' do
 
-  let(:user_attrs) { attributes_for(:user) }
+  describe "signing up new users" do
+    let(:user_attrs) { attributes_for(:user) }
 
-  it "should permit signing up Users" do
-    visit new_user_registration_path
-    within("#new_user") {
-      fill_in :name, with:user_attrs[:name]
-      fill_in :email, with:user_attrs[:email]
-      fill_in :password, with:user_attrs[:password]
-      fill_in :password_confirmation, with:user_attrs[:password_confirmation]
-    }
-    expect {
-      click_button "Sign up"
-    }.to change(User, :count).by(1)
+    it "should permit signing up Users" do
+      visit new_user_registration_path
+      within("#new_user") {
+        fill_in :name, with:user_attrs[:name]
+        fill_in :email, with:user_attrs[:email]
+        fill_in :password, with:user_attrs[:password]
+        fill_in :password_confirmation, with:user_attrs[:password_confirmation]
+      }
+      expect {
+        click_button "Sign up"
+      }.to change(User, :count).by(1)
 
+    end
   end
 
   context "user is signed in" do
